5b10cc2a954f615382de6dd8d35d204ae4bcdf32
Виктория Федорова <fiodorova.vika301296@yandex.ru>
2023-07-20 15:56:37 +0300

    commenting testing code

--- cakes/views.py
+++ cakes/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+# from django.shortcuts import render
 
-# Create your views here.
+# def index(request):
+#     template = 'index.html'
+#     title = 'Hello, world!'
+#     context = {'title': title,}
+#     return render(request, template, context)
+
