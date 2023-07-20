f3edaf0dd96c04375f391732995b89906ab41843
Виктория Федорова <fiodorova.vika301296@yandex.ru>
2023-07-20 15:13:29 +0300

    changing index.html, adding folders of templates and static, changing settings.py

--- project/settings.py
+++ project/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
+import os
 
 from pathlib import Path
 
@@ -67,10 +68,11 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = "project.urls"
 
+TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [TEMPLATES_DIR],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
