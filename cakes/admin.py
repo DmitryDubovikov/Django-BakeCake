7e6416f85e5c801c3c004349a5ebf21beeb8ad2e
0viktory0 <36894469+0viktory0@users.noreply.github.com>
2023-07-21 00:30:51 +0300

    added admin

--- cakes/admin.py
+++ cakes/admin.py
@@ -1,3 +1,72 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Berry, Cake, Decor, Level, Order, Shape, Topping
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = [
+        'address',
+        'date',
+        'time',
+        'comment',
+        'status',
+        'client'
+    ]
+    list_filter = [
+        'status',
+    ]
+
+
+@admin.register(Cake)
+class CakeAdmin(admin.ModelAdmin):
+    list_display = [
+        'name',
+        'levels',
+        'shape',
+        'topping',
+        'berries',
+        'decor',
+        'inscription',
+        'image'
+    ]
+
+
+@admin.register(Level)
+class CakeLevelAdmin(admin.ModelAdmin):
+    list_display = [
+        'title',
+        'price'
+    ]
+
+
+@admin.register(Shape)
+class CakeShapeAdmin(admin.ModelAdmin):
+    list_display = [
+        'title',
+        'price'
+    ]
+
+
+@admin.register(Topping)
+class CakeToppingAdmin(admin.ModelAdmin):
+    list_display = [
+        'title',
+        'price'
+    ]
+
+
+@admin.register(Berry)
+class CakeBerryAdmin(admin.ModelAdmin):
+    list_display = [
+        'title',
+        'price'
+    ]
+
+
+@admin.register(Decor)
+class CakeDecorAdmin(admin.ModelAdmin):
+    list_display = [
+        'title',
+        'price'
+    ]
