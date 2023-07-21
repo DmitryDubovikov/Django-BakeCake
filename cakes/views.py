f31ce140d8703ca2e3a9bbe54c8522a150ad67cb
DmitryDubovikov <dmitry.dubovikov@gmail.com>
2023-07-21 16:40:16 -0300

    add cake customization

--- cakes/views.py
+++ cakes/views.py
@@ -1,10 +1,17 @@
 from django.shortcuts import render
+from .models import Berry, Decor, Topping, Shape, Level
 
 
 def index(request):
     template = "index.html"
     title = "Hello, world!"
+
     context = {
         "title": title,
+        "berries": Berry.objects.all(),
+        "decors": Decor.objects.all(),
+        "toppings": Topping.objects.all(),
+        "shapes": Shape.objects.all(),
+        "levels": Level.objects.all(),
     }
     return render(request, template, context)
